6cb196262bb84772abb817d93fd5c26d27dbe3a2
AWAIS-514 (tb) <meet2freelancing@gmail.com>
2024-05-13 13:28:06 +0500

    boilerplate initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>Hello Tayyab! deploying a boiler plate App!</p>
+    </main>
+  );
+}
