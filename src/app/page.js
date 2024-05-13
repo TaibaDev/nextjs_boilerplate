1e99d6b8a01e6c0d1a86ae12208ae9af88d06b4a
AWAIS-514 (tb) <meet2freelancing@gmail.com>
2024-05-13 14:13:55 +0500

    BG applied

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="py-52 text-3xl bg-pink-500 text-white px-52 rounded-lg flex flex-col">
+    <main className="flex min-h-screen bg-pink-500 flex-col items-center justify-between p-24">
+      <div className="py-52 text-3xl  text-white px-52 rounded-lg flex flex-col">
         <p>Hello Tayyab! are you deploying a boiler plate NextJS App, Thats great!!!</p>
         <button className="px-3 py-2 rounded-md mx-auto bg-white text-black my-5 hover:bg-pink-300 hover:text-white">Click me</button>
       </div>
