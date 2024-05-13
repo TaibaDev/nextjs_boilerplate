9cf8e1e3be985c197f17fe9ccb04cc8b75803123
AWAIS-514 (tb) <meet2freelancing@gmail.com>
2024-05-13 13:34:43 +0500

    page.js updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>Hello Tayyab! deploying a boiler plate App!</p>
+      <div className="py-52 text-3xl bg-pink-500 text-white px-52 rounded-lg">
+        <p>Hello Tayyab! are you deploying a boiler plate NextJS App, Thats great!</p>
+      </div>
     </main>
   );
 }
